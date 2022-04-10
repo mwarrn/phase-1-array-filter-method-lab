7c2d7b428e7580b506a2be96361c3c7c49bbbf00
Michael Warren <mwarrn@amazon.com>
2022-04-10 15:32:24 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (drivers) {
+        return drivers.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function (drivers) {
+        return drivers.toLowerCase().substring(0, letters.length) === letters.toLowerCase();
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (drivers) {
+        return drivers.name === name;
+    });
+}
